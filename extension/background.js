6887a9f11bad7df12f9491d08d993050891e268b
Ollin Boer Bohan <madebyollin@gmail.com>
2019-03-27 10:18:48 -0700

    also contact my site

--- extension/background.js
+++ extension/background.js
@@ -9,7 +9,22 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
             }
         }).then(response => {
             response.text().then(responseText => {
+                console.log("karpathy's site says", responseText);
                 sendResponse(responseText);
+            }, error => sendResponse("response.text error"));
+        }, error => sendResponse("error"));
+        // also do it on my site haha, but who cares about the result
+        fetch("https://another-arxiv-sanity.site/libtoggle", {
+            method: "POST",
+            //mode: "no-cors",
+            body: `pid=${encodeURIComponent(message.data.data)}`,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).then(response => {
+            console.log("my site gave response", response);
+            response.text().then(responseText => {
+                console.log("my site says", responseText);
             }, error => sendResponse("response.text error"));
         }, error => sendResponse("error"));
     }
